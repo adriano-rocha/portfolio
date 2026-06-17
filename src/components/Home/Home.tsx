e734a434399c25d683e73505ca306fddb50243f4
Adriano Rocha <adrianorocha.dev@gmail.com>
2026-06-17 11:22:18 -0300

    feat: adicionar nova foto de perfil

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -83,7 +83,7 @@ function Home() {
         <div className="relative">
           <div className="w-64 h-64 sm:w-72 sm:h-72 lg:w-80 lg:h-80 rounded-full overflow-hidden shadow-lg border-4 border-emerald-400">
             <img
-              src="/images/me_new.jpeg"
+              src="/images/my-foto-page.png"
               alt="Adriano Rocha"
               className="w-full h-full object-cover object-center"
             />
